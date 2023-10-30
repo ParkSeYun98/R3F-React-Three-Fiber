bf320f2c716afdbcb1673b04fdf003c2ca64118b
ParkSeYun98 <yun12343@naver.com>
2023-10-30 10:22:54 +0900

    Material 1

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ import MyElement3D from './Component/MyElement3D'
 function App() {
   return (
     <>
-      <Canvas>
+      {/* camera로부터 거리가 3.5인 픽셀지점은 그 값을 0으로 할당, 
+          camera로부터 거리가 6인 지점은 2를 할당 */}
+      <Canvas camera = {{near: 3.5, far: 6}} >
         <MyElement3D/>
       </Canvas>   
     </>
--- src/Component/MyElement3D.jsx
+++ src/Component/MyElement3D.jsx
@@ -1,5 +1,6 @@
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, useTexture } from "@react-three/drei";
 import { useEffect, useRef } from "react";
+import { useControls } from "leva";
 import * as THREE from "three"
 
 function MyElement3D() {
@@ -10,20 +11,40 @@ function MyElement3D() {
         mesh2.current.material = mesh1.current.material
     }, [])
 
+    // const {roughness, metalness, clearcoat, clearcoatRoughness, transmission, thickness, ior} = useControls({
+    //     roughness: {value: 0, min: 0, max: 1, step: 0.01},
+    //     metalness: {value: 0, min: 0, max: 1, step: 0.01},
+    //     clearcoat: {value: 0, min: 0, max: 1, step: 0.01}, 
+    //     clearcoatRoughness: {value: 0, min: 0, max: 1, step: 0.01},
+    //     transmission: {value: 0, min: 0, max: 1, step: 0.01},
+    //     thickness: {value: 0, min: 0, max: 1, step: 0.01},
+    //     ior: {value: 1.5, min: 0, max: 2.333, step: 0.01}
+    // })
+
+    // matcap 이미지 불러오기
+    // const matcap = useTexture("././public/images/matcap.jpg")
+
+    // tone 불러오기
+    const texture = useTexture("././public/images/threeTone.jpg")
+    // 색상 보관이 이루어지지 않도록 함. 
+    texture.minFilter = THREE.NearestFilter
+    texture.magFilter = THREE.NearestFilter
+
     return (
         <>
             <OrbitControls />
 
             <ambientLight intensity = {0.2} />
 
-            <directionalLight position = {[0, 1, 0]} />
-            <directionalLight position = {[1, 2, 8]} intensity = {0.7} />
+            {/* meshMatcapMaterial은 광원을 필요로 하지 않는다. */}
+            {/* <directionalLight position = {[0, 1, 0]} />
+            <directionalLight position = {[1, 2, 8]} intensity = {0.7} /> */}
 
             <mesh 
                 ref = {mesh1}
                 position = {[0.7, 0, 0]}
             >
-                <boxGeometry />
+                <torusKnotGeometry args = {[0.5, 0.15, 256, 128]} />
 
                 {/* 
                 visible : 보이는지 안보이는지 true/false
@@ -59,7 +80,7 @@ function MyElement3D() {
                     color = "#d25383" 
                 /> */}
 
-                <meshPhongMaterial
+                {/* <meshPhongMaterial
                     wireframe = {false}
                     visible={true}
                     transparent = {true}
@@ -78,7 +99,68 @@ function MyElement3D() {
 
                     // mesh를 평평하게 렌더링할 지 여부
                     flatShading = {true}
-                />
+                /> */}
+
+                {/* <meshStandardMaterial 
+                    visible={true}
+                    transparent = {true}
+                    opacity = {0.5}
+                    depthTest = {true}
+                    depthWrite = {true}
+                    side = {THREE.FrontSide}
+
+                    emissive = {0x00000}
+                    flatShading = {false}
+
+                    color = {0xff0000} 
+                    wireframe = {false}
+
+                    // meshStandardMaterial 고유 특성
+                    // roughness : 거칠기
+                    // metalness : 금속성
+                    roughness = {roughness}
+                    metalness = {metalness}
+                /> */}
+
+                {/* <meshPhysicalMaterial 
+                    visible={true}
+                    transparent = {true}
+                    opacity = {0.5}
+                    depthTest = {true}
+                    depthWrite = {true}
+                    side = {THREE.DoubleSide}
+                    emissive = {0x00000}
+                    flatShading = {false}
+                    roughness = {roughness}
+                    metalness = {metalness}
+
+                    color = {0xffffff} 
+                    wireframe = {false}
+
+                    // meshPhysicalMaterial 고유 속성
+                    // clearcoat : 코팅된 광택 있는 층
+                    // clearcoatRoughness :  clearcoat 표면의 거칠기
+                    clearcoat = {clearcoat}
+                    clearcoatRoughness = {clearcoatRoughness}
+
+                    // meshPhysicalMaterial 유리에 사용되는 고유 속성
+                    // transmission : 유리투명도
+                    // thickness : 유리 두께
+                    // ior : 유리 굴절률
+                    transmission = {transmission}
+                    thickness = {thickness}
+                    ior = {ior}
+                /> */}
+
+                {/* <meshDepthMaterial/> */}
+
+                {/* flatShading : 각진 면으로 표현 */}
+                {/* <meshMatcapMaterial matcap={matcap} 
+                    flatShading = {true}
+                /> */}
+
+                <meshToonMaterial gradientMap={texture} color="cyan" />
+                
             </mesh>
 
             <mesh 
