4c800431e82a29c58ac60402b332cc1adb32978b
ParkSeYun98 <yun12343@naver.com>
2023-10-31 14:34:03 +0900

    3D Model Animation

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,13 @@ import MyElement3D from './Component/MyElement3D'
 function App() {
   return (
     <>
-      {/* 기본 카메라 : Perspective Camera */}
-      {/* 카메라 렌즈 화각 : 75도
-          카메라 위치 : 7, 7, 0 
-      
-      
-      */}
       <Canvas 
-        orthographic
         camera = {{
-          near: 0.1,
-          far: 20,
-          position: [7, 7, 0],
-          zoom: 100
+          near: 1,
+          far: 100,
+          position: [7, 7, 0]
       }} >
-        <MyElement3D/>
+        <MyElement3D />
       </Canvas>   
     </>
   )
